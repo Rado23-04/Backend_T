314e24148d9ced87c48af0427236bdc7b6245d2d
Rado23-04 <hei.rado.2@gmail.com>
2025-02-17 22:00:40 +0300

    feat: find by email repository

--- src/main/java/com/back/TaskFlow/Repository/RepositoryUser.java
+++ src/main/java/com/back/TaskFlow/Repository/RepositoryUser.java
@@ -4,6 +4,9 @@ import com.back.TaskFlow.Models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface RepositoryUser extends JpaRepository<User,Long> {
+    Optional<User> findByEmail(String email);
 }
