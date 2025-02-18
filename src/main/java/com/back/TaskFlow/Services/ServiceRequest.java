0545f5d9a6eebf68b8ef52e7314ca00bcc2cc9d3
Rado23-04 <hei.rado.2@gmail.com>
2025-02-18 09:57:48 +0300

    chore: pluggins lombok

--- src/main/java/com/back/TaskFlow/Services/ServiceRequest.java
+++ src/main/java/com/back/TaskFlow/Services/ServiceRequest.java
@@ -3,6 +3,7 @@ package com.back.TaskFlow.Services;
 import com.back.TaskFlow.Models.User;
 import com.back.TaskFlow.Repository.RepositoryUser;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -13,12 +14,18 @@ public class ServiceUser {
     @Autowired
     private RepositoryUser repositoryUser;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     public boolean CheckAuthenticate (String email,String password){
         Optional<User> optionalUser = repositoryUser.findByEmail(email);
 
         if(optionalUser.isPresent()){
             User user = optionalUser.get();
-
+                if(passwordEncoder.matches(password,user.getPassword()){
+                    return true;
+            }
+            return false;
         }
     }
 }
