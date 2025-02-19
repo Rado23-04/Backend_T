fab459e3aec25a543d9075b9780aec05f8db131d
Rado23-04 <hei.rado.2@gmail.com>
2025-02-19 16:59:17 +0300

    fix: Hash user password before saving to database

--- src/main/java/com/back/TaskFlow/Services/ServiceUser.java
+++ src/main/java/com/back/TaskFlow/Services/ServiceUser.java
@@ -3,6 +3,7 @@ package com.back.TaskFlow.Services;
 import com.back.TaskFlow.Models.User;
 import com.back.TaskFlow.Repository.RepositoryUser;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -10,7 +11,12 @@ public class ServiceUser {
     @Autowired
     private RepositoryUser repositoryUser;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     public User saveUser(User user){
+        String hashedPassword = passwordEncoder.encode(user.getPassword());
+        user.setPassword(hashedPassword);
         return repositoryUser.save(user);
     }
 }
