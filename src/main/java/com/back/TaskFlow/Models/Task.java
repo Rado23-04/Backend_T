6eb56b009e3e00ecaf3a37c8ce77513663b96ef2
Rado23-04 <hei.rado.2@gmail.com>
2025-02-21 22:17:19 +0300

    feat: modele task

---
+++ src/main/java/com/back/TaskFlow/Models/Task.java
@@ -0,0 +1,39 @@
+package com.back.TaskFlow.Models;
+
+import com.back.TaskFlow.enumm.Priority;
+import com.back.TaskFlow.enumm.Status;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Data
+@Table(name = "task")
+@Builder
+public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+
+    @Enumerated(EnumType.STRING)
+    private Status status;
+
+    @Enumerated(EnumType.STRING)
+    private Priority priority;
+
+    private Date date;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+}
