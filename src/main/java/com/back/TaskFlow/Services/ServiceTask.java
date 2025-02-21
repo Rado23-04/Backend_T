acef3aeb2ca7f1b2ea1991aabcc2fc71df50e335
Rado23-04 <hei.rado.2@gmail.com>
2025-02-21 22:54:41 +0300

    feat: implement task service

---
+++ src/main/java/com/back/TaskFlow/Services/ServiceTask.java
@@ -0,0 +1,32 @@
+package com.back.TaskFlow.Services;
+
+import com.back.TaskFlow.Models.Task;
+import com.back.TaskFlow.Repository.RepositoryTask;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ServiceTask {
+
+    @Autowired
+    private RepositoryTask repositoryTask;
+
+    public List<Task> getAllTask(){
+         return repositoryTask.findAll();
+    }
+
+    public Optional<Task> getTask(String title){
+        return repositoryTask.findByName(title);
+    }
+
+    public String DeleteTask (Long id){
+         if(!repositoryTask.existsById(id)){
+             return "Id doesn't exist"+id;
+         }
+         repositoryTask.deleteById(id);
+         return "Task delete";
+    }
+}
