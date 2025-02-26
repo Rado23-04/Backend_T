88f05a713c9376bc3118a5230a80c50fc321806f
Rado23-04 <hei.rado.2@gmail.com>
2025-02-26 14:54:58 +0300

    feat: function find by title

--- src/main/java/com/back/TaskFlow/Repository/RepositoryTask.java
+++ src/main/java/com/back/TaskFlow/Repository/RepositoryTask.java
@@ -8,5 +8,5 @@ import java.util.Optional;
 
 @Repository
 public interface RepositoryTask extends JpaRepository<Task,Long> {
-    Optional<Task> findByName (String title);
+    Optional<Task> findByTitle (String title);
 }
